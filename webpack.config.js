b33737c72f98170d11b1c7c4d92497f98bf794e2
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-06 16:18:53 -0500

    optimization complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,52 @@
 const path = require("path"); // adding the Node module
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 
 module.exports = {
-    entry: './assets/js/script.js', // where webpack starts building module
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    }, // where webpack starts building module
     output: {
-        path: path.resolve(__dirname, 'dist'), // where webpack builds filesand their name
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ],
+            }
+        ],
+    },
+
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 
